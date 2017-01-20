a71ef245cc06affdf27b1158435e2dc6fb42e6f5
padilla410 <padilla410@gmail.com>
2017-01-20 14:50:00 -0500

    First complete draft of component analysis plots
    Using results from RVAJR02_079 and RVAJR_C01

--- Component_Analysis/02_Summarize_EFDC_Output.R
+++ Component_Analysis/02_Summarize_EFDC_Output.R
@@ -10,8 +10,8 @@ dat$year_month <- with(dat, paste(year(datetime), '-'
 dat_monthly_stv_std_efdc <- dat %>% 
   group_by(year_month) %>% 
   dplyr::summarise(ct = n(), gtr_235_ct = sum(downstream_boundary > 235)) %>% 
-  mutate(perc_235_violation = gtr_235_ct / ct * 100) %>% 
-  select(year_month, perc_235_violation)
+  mutate(perc_235_violation_efdc = gtr_235_ct / ct * 100) %>% 
+  select(year_month, perc_235_violation_efdc)
 
 #temporary addition (17 months)
 # dat_stv_check <- dat_monthly_stv_std_efdc %>% filter(perc_235_violation > 10)
@@ -21,7 +21,7 @@ dat_monthly_geomean_efdc <- dat %>% group_by(datetime) %>%
   mutate(ln_total = log(downstream_boundary))
   
 dat_monthly_geomean_efdc <- dat_monthly_geomean_efdc %>% group_by(year_month) %>% 
-  dplyr::summarise(total = exp(mean(ln_total)))
+  dplyr::summarise(total_efdc = exp(mean(ln_total)))
 
 #temporary addition (4 months)
 # dat_geomean_check <- dat_monthly_geomean_efdc %>% filter(total > 126)
